eda9e9b869686bfe5592c285087a6511a1e53c8c
scvd03 <scvd03@gmail.com>
2022-01-06 17:37:35 +0900

    ✨  Add CheseMonster Function(Champion)

--- HW-1/Assets/Scripts/Champion/Champion.cs
+++ HW-1/Assets/Scripts/Champion/Champion.cs
@@ -7,8 +7,34 @@ public class Champion : MonoBehaviour
   public float damage = 10f;
   public float speed = 50f;
 
+  private Monster nearestMonster = null;
+
   private void Start() {
-    EventManager.DieEvent += Die;
+  }
+  void Update () {
+		ChaseMonster();
+	}
+
+  // TODO: 몬스터가 죽을 때마다(이벤트) 업데이트되는 배열을 제공받으면 좋을 거 같음
+	void FindClosestEnemy()
+	{
+		float distanceToClosestEnemy = Mathf.Infinity;
+		Monster[] allEnemies = GameObject.FindObjectsOfType<Monster>();
+
+		foreach (Monster currentEnemy in allEnemies) {
+			float distanceToEnemy = (currentEnemy.transform.position - this.transform.position).sqrMagnitude;
+			if (distanceToEnemy < distanceToClosestEnemy) {
+				distanceToClosestEnemy = distanceToEnemy;
+				this.nearestMonster = currentEnemy;
+			}
+		}
+	}
+
+  // TODO: 추적함수는, 자신이 공격하는 몬스터가 null이 되었으면 호출하는 것이 좋을 거 같음
+  void ChaseMonster() {
+    FindClosestEnemy();
+
+    transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), this.nearestMonster.transform.position, 3 * Time.deltaTime);
   }
 
   public void Die() {
@@ -16,6 +42,6 @@ public class Champion : MonoBehaviour
   }
 
   private void OnDestroy() {
-    EventManager.DieEvent -= Die;
+    // EventManager.DieEvent -= Die;
   }
 }
