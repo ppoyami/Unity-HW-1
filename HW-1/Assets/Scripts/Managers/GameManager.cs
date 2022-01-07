131605a81911aa3b8cf790886f386e46d9a29f5d
scvd03 <scvd03@gmail.com>
2022-01-07 15:49:55 +0900

    ✨  Add Upgrade function

--- HW-1/Assets/Scripts/Managers/GameManager.cs
+++ HW-1/Assets/Scripts/Managers/GameManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     private static GameManager _instance;
-    public SpawnManager spawnManager;
 
+    public SpawnManager spawnManager;
+    public Text GoldText;
+
+    int gold = 0;
     bool inCombat = false;
 
     public static GameManager Instance {
@@ -33,11 +37,47 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Start() {
+      EventManager.MonsterDieEvent += UpdateGoldText;
+      this.UpdateGoldText();
+    }
+
+    public void OnDamageUp() {
+      if (this.gold < 10) return;
+
+      GameObject championObj = GameObject.FindGameObjectWithTag("Champion");
+      Champion champion = championObj.GetComponent<Champion>();
+      champion.damage += 1.1f;
+      this.gold -= 10;
+      this.UpdateGoldText();
+      Debug.Log("upgrade" + champion.damage);
+    }
+
+    public void OnSpeedUp() {
+      if (this.gold < 10) return;
+      
+      GameObject championObj = GameObject.FindGameObjectWithTag("Champion");
+      Champion champion = championObj.GetComponent<Champion>();
+      champion.speed += 1.1f;
+      this.gold -= 10;
+      this.UpdateGoldText();
+      Debug.Log("upgrade" + champion.speed);
+    }
+
     public void OnRelocate() {
       this.ResetObjects();
       spawnManager.RandomSpawnMonster();
       inCombat = false;
     }
+
+    public void setGold(int value) {
+      this.gold += value;
+    }
+
+    void UpdateGoldText() {
+      GoldText.text = System.String.Format("Gold: {0:n0}", this.gold);
+    }
+
 
     void ResetObjects() {
       GameObject[] champions = GameObject.FindGameObjectsWithTag("Champion");
